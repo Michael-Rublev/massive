bec8cbe2bb280bc9ebebd735880902ec1ea8c6cd
Michael <m.cmsrus@gmail.com>
2022-11-08 18:34:03 +0100

    Massive, home work

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] nums = new int[10]; //
+//        nums[0] = 1;
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.println(nums[i]);
+
+//        //create new massive
+//        int[] array = {22, 4, 5, 6};
+//        //Method how to make new copy this massive
+//        int[] copy = Arrays.copyOf(array, array.length);
+//        System.out.println(copy);
+
+
+//        //create new massive
+//        int[] array = {22, 4, 5, 6};
+//        //Make new copy of the size
+//        int[] copy = Arrays.copyOfRange(array, 1, 3);
+//        //copy will be = {4, 5}.
+//
+
+//        //create new massive
+//        int[] array = {22, 4, 5, 6};
+//        //Entry massive in console
+//        System.out.println(Arrays.toString(array));
+//        //sout one string all massive: [22, 4, 5, 6]
+
+
+//        create new massive
+//        int[] array = {22, 4, 5, 6};
+          //sort massive
+//        Arrays.sort(array);
+          //console : [4, 5, 6, 22]
+
+        //new massive
+//        int[] array = {4, 5, 6, 22};
+        //find element and view here index in console
+//        System.out.println(Arrays.binarySearch(array, 4));
+        //index = 0.
+
+        //new massive
+//        int[] array = new int[10];
+        //fill array all massive case num '15'
+//        Arrays.fill(array, 15);
+
+        //new massive 1
+//        int[] array1 = {1, 2, 4, 56};
+        //new massive 2
+//        int[] array2 = {1, 762, 44, 5};
+//        equals massive 1 == massive 2 true/false
+//        Arrays.equals(array1, array2);
+        //massive is not equals, console log = false
+//
+
+    }
+}
